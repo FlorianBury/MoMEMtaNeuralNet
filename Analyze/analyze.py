c8a3f280f7ffabfbbbbfc92bfce1adcf5f2dac45
FlorianBury <FlorianBury@github.com>
2020-03-15 16:17:42 +0100

    In analyze module, added LRP, combined plot and prototype is fully functionnal

--- Analyze/analyze.py
+++ Analyze/analyze.py
@@ -3,28 +3,33 @@ import sys
 import logging
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot  as plt
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import seaborn as sns
 import argparse
 import parse
+import pickle
 from collections import OrderedDict 
 
 from sklearn.feature_selection import SelectKBest, f_regression, mutual_info_regression
 from scipy.stats import pearsonr
+from keras.models import model_from_json
+
+import innvestigate
+import innvestigate.utils as iutils
 
 sys.path.append(os.path.abspath('..'))
 import parameters
 
 
 class Analyze:
-    def __init__(self,N,suffix,model=None):
+    def __init__(self,N,suffix):
         self.inputs     = parameters.inputs
         self.outputs    = parameters.outputs
         self.path_files = parameters.path_gen_output
         self.path_out   = os.path.abspath(".")
         self.N          = N
         self.suffix     = suffix
-        self.model      = model
         self.scores     = pd.DataFrame(columns=self.inputs)
 
         self.makeVarNames()
@@ -93,14 +98,25 @@ class Analyze:
     def getDF(self):
         from import_tree import LoopOverTrees
         df = LoopOverTrees(input_dir  = self.path_files,
-                           variables = self.inputs+self.outputs,
-                           n         = self.N)
+                           variables  = self.inputs+self.outputs,
+                           n          = self.N)
         self.df_inputs = df[self.inputs]
         self.df_outputs = df[self.outputs]
         del df
         logging.info("Number of events in sample : %d"%(self.df_inputs.shape[0]))
         logging.info("Number of input variables  : %d"%(self.df_inputs.shape[1]))
         logging.info("Number of output variables : %d"%(self.df_outputs.shape[1]))
+
+    def loadModel(self,basemodel):
+        # Need to find basemodel.json and basemodel.h5 #
+        self.basemodel = basemodel
+        with open(basemodel+'.json','r') as f:
+            loaded_model_json = f.read()
+        self.model = model_from_json(loaded_model_json)
+        self.model.load_weights(basemodel+".h5")
+        # Load scaler as basemodel.pkl#
+        with open(basemodel+'.pkl', 'rb') as handle:
+            self.scaler = pickle.load(handle)
 
     def makeInputsGridPlot(self):
         logging.info("Starting the input map plot")
@@ -164,56 +180,183 @@ class Analyze:
         #logging.info("... saved as %s"%name)
 
     def outputsCorrelation(self):
+        logging.info("Processing correlation")
         scores = []
         for inp in self.inputs:
             r = pearsonr(self.df_inputs[inp].values,self.df_outputs.values.ravel()) 
             scores.append(r[0])
         new_row = pd.DataFrame(np.array(scores).reshape(1,-1),columns=self.scores.columns,index=["Pearson correlation"])
         self.scores = self.scores.append(new_row,verify_integrity=True)
+        logging.info("... done")
 
     def outputsMutualInformation(self):
+        logging.info("Processing mutual information")
         scores = []
         for inp in self.inputs:
             r = mutual_info_regression(self.df_inputs[inp].values.reshape(-1,1),self.df_outputs.values.ravel())
             scores.append(r[0])
         new_row = pd.DataFrame(np.array(scores).reshape(1,-1),columns=self.scores.columns,index=["Mutual information"])
         self.scores = self.scores.append(new_row,verify_integrity=True)
+        logging.info("... done")
 
     def extractFirstLayerWeights(self):
+        logging.info("Processing network first layer averaged sum")
         import h5py
-        f = h5py.File(self.model,'r')
+        f = h5py.File(self.basemodel+'.h5','r')
         weights = f['dense_1']['dense_1_1']['kernel:0'][()]
         avg_sum_weights = np.sum(np.absolute(weights),axis=1)/weights.shape[1]
         new_row = pd.DataFrame(avg_sum_weights.reshape(1,-1),columns=self.scores.columns,index=['First layer weights averaged sum'])
         self.scores = self.scores.append(new_row,verify_integrity=True)
+        logging.info("... done")
+
+    def makeInputsVariations(self,inputs,index,varmax,varstep):
+        # Generate all variations of input at index #
+        valueToVary = inputs[index]
+        allvars = np.arange(valueToVary-varmax,valueToVary+varmax,varstep)
+        nvar = allvars.shape[0]
+        # Generate full array #
+        arrayLeft  = np.tile(inputs[0:index],(nvar,1))
+        arrayRight = np.tile(inputs[index+1:],(nvar,1))
+        arrayFull  = np.c_[arrayLeft,allvars,arrayRight]
+        return arrayFull
+
+    def differentiateNetwork(self):
+        logging.info("Processing network differentiation")
+        bias = np.zeros(self.df_inputs.shape[1])
+        std  = np.zeros(self.df_inputs.shape[1])
+
+        Nevents = 100000
+        for i in range(min(Nevents,self.df_inputs.shape[0])): # Loop over events 
+            print (i)
+            # Get one event entry and scale it #
+            inputs = self.df_inputs.loc[i].values.reshape(1,-1)
+            inputs = self.scaler.transform(inputs)
+            output = self.df_outputs.loc[i].values[0]
+            # Loop over each variable #
+            for j in range(inputs.shape[1]):
+                inputsvar = self.makeInputsVariations(inputs  = inputs[0], # Inputs is 2D
+                                                      index   = j,
+                                                      varmax  = 5,
+                                                      varstep = 0.1) 
+                outputsvar = self.model.predict(inputsvar,batch_size=inputsvar.shape[1])
+                bias[j] += np.mean(outputsvar)-output
+                std[j]  += np.std(outputsvar)
+            
+        bias /= Nevents
+        std  /= Nevents
+
+        bias_row = pd.DataFrame(bias.reshape(1,-1),columns=self.scores.columns,index=['Derivative bias'])
+        std_row = pd.DataFrame(std.reshape(1,-1),columns=self.scores.columns,index=['Derivative std'])
+        self.scores = self.scores.append(bias_row,verify_integrity=True)
+        self.scores = self.scores.append(std_row,verify_integrity=True)
+        logging.info("... done")
+
+    def LRPAnalysis(self):
+        logging.info("Processing network LRP")
+        analyzers = {"Guided backpropagation":"guided_backprop","Input gradient":"input_t_gradient","LRP (z method)":"lrp.z"}
+        inputs = self.scaler.transform(self.df_inputs.values)
+        for title,analyzer_func in analyzers.items():
+            analyzer = innvestigate.create_analyzer(analyzer_func,self.model)
+            analysis = analyzer.analyze(inputs)
+            result   = np.sum(analysis,axis=0)/analysis.shape[0]
+            new_row = pd.DataFrame(result.reshape(1,-1),columns=self.scores.columns,index=["LRP : "+title])
+            self.scores = self.scores.append(new_row,verify_integrity=True)
+        logging.info("... done")
 
     def plotScores(self):
         # Calling basic score functions #
-        logging.info("Processing correlation")
         self.outputsCorrelation()
-        logging.info("Processing mutual information")
         self.outputsMutualInformation()
+
         logging.info("Starting the score plot")
-
-
+        # --- separate bar plots --- #
         # Plotting options  #
-        fig, ax = plt.subplots(self.scores.shape[0],figsize=(18,18))
+        N = self.scores.shape[0]
+        color=iter(plt.cm.rainbow(np.linspace(0.3,1,N)))                                                                                                                                                
+        fig, ax = plt.subplots(N,figsize=(18,N*6))
         fig.subplots_adjust(left=0.1, right=0.9, top=0.95, bottom=0.1,hspace=0.4)
-        width = 0.3
         bins = np.arange(self.scores.shape[1])
         labels = []
         for col in self.scores.columns:
             labels.append(self.inputs_names[col])
 
-        # Plot the different scors #
+        # Plot the different scores separately  #
         for i,(index, row) in enumerate(self.scores.iterrows()): # Index is title, row is data
-            ax[i].bar(bins,row,tick_label=labels)
+            c = next(color)
+            #ax[i].bar(bins,row,tick_label=labels,color=c)
+            row = np.abs(row) #  TODO : might not be correct
+            ax[i].bar(bins,(row-np.amin(row))/(np.amax(row)-np.amin(row)),tick_label=labels,color=c)
             ax[i].set_xticklabels(labels,rotation='vertical')
             ax[i].set_title(index,fontsize=20)
 
-        name = os.path.join(self.path_out,'scores_%s.png'%self.suffix)
+        name = os.path.join(self.path_out,'separateScores_%s.png'%self.suffix)
         plt.savefig(name)
         logging.info("... saved as %s"%name)
+
+        # --- separate bar plots --- #
+        # Plotting options  #
+        fig, ax = plt.subplots(1,figsize=(36,9))
+        fig.subplots_adjust(left=0.01, right=0.99, top=0.95, bottom=0.1,hspace=0.2)
+        color=iter(plt.cm.rainbow(np.linspace(0.3,1,N)))                                                                                                                                                
+
+        # Define binning #
+        bin_dict = {} # Will contain the bins for each hist
+        i = 0
+        for name in self.scores.index:
+            if i==0:
+                bin_dict[name] = np.arange(self.scores.shape[1])
+            else:
+                bin_dict[name] = np.arange(self.scores.shape[1])+i/(N)
+            i += 1
+        
+        # Loop over data and plot #
+        for i,(index, row) in enumerate(self.scores.iterrows()): # Index is title, row is data
+            c = next(color)
+            row = np.abs(row) #  TODO : might not be correct
+            ax.bar(bin_dict[index],
+                        #row/np.sum(row),
+                        (row-np.amin(row))/(np.amax(row)-np.amin(row)),
+                        align ='edge',
+                        width = 1/N,
+                        color = c,
+                        linewidth = 2,
+                        label = index,
+                        tick_label=None)
+                        #tick_label=labels)
+        ax.set_xlim(0,self.scores.shape[1])
+        ax.set_xticks(np.arange(self.scores.shape[1])+0.5)
+        ax.set_xticklabels(labels)
+
+        # Vertical lines at ticks #
+        for i in range(1,self.scores.shape[1]):
+            plt.axvline(x=i, color='k', linestyle='--')
+
+        plt.legend(loc='upper right')
+        name = os.path.join(self.path_out,'combineScores_%s.png'%self.suffix)
+        plt.savefig(name)
+        logging.info("... saved as %s"%name)
+        
+#     for masspoint in list(inter_dict.values())[0].keys(): # Loop over mass points of first dict
+#         print ('Masspoint : m_H = %0.2f, m_A = %0.2f'%(masspoint[1],masspoint[0]))
+#         color=iter(plt.cm.rainbow(np.linspace(0.3,1,N)))                                                                                                                                                
+#         fig = plt.figure()
+#         for name, hist_dict in inter_dict.items(): # Loop over the histograms dict
+#             # Plot the bars #
+#             c=next(color)
+#             p = plt.bar(bin_dict[name],
+#                     hist_dict[masspoint],
+#                     align='edge',
+#                     width=0.5/(N+1),
+#                     color=c,
+#                     linewidth=2,
+#                     label=name)
+# 
+#         # Optional parameters #
+#         plt.legend(loc='upper right')
+#         plt.xlabel(r'$\rho$')
+#         plt.ylabel('Arb. units')
+#         plt.title(r'Mass point $m_H=$%d GeV, $m_A$=%d GeV'%(masspoint[1],masspoint[0]))
+ 
 
 
 
@@ -235,7 +378,7 @@ if __name__ == '__main__':
     outputsplots.add_argument('-s','--scoreplot', action='store_true', required=False, default=False,
         help='Wether to plot the scores')
     outputsplots.add_argument('-m','--model', action='store', required=False, default=None, type=str,
-        help='Model name, required for the arguments looking at the network')
+        help='Model name (base + .json and +.h5), required for the arguments looking at the network')
     outputsplots.add_argument('--firstlayer', action='store_true', required=False, default=False,
         help='Wether to plot the sum of the weights of the first layer')
     outputsplots.add_argument('--LRP', action='store_true', required=False, default=False,
@@ -254,18 +397,20 @@ if __name__ == '__main__':
         logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s',datefmt='%m/%d/%Y %H:%M:%S')
     #--- Instantiation ---#
     instance = Analyze(N      = opt.number,
-                       suffix = opt.suffix,
-                       model  = opt.model)
+                       suffix = opt.suffix)
+    if opt.model is not None:
+        instance.loadModel(opt.model)
     if opt.correlation:
         instance.makeCorrelationMatrix()
     if opt.gridplot:
         instance.makeInputsGridPlot()
     if opt.firstlayer: # Needs to be before plotScores()
         instance.extractFirstLayerWeights()
+    if opt.derivative: # Needs to be before plotScores() 
+        instance.differentiateNetwork()
+    if opt.LRP: # Needs to be before plotScores() 
+        instance.LRPAnalysis()
     if opt.scoreplot:
         instance.plotScores()
 
-
-# f['dense_1']['dense_1_1']['kernel:0'].value
-# np.sum(np.absolue(a),axis=1)/200
 
